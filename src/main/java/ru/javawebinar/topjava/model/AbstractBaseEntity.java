0b24529e9333fb1d618dd76beb11ac5ce0e19c47
buchgit <buchnorilsk@gmail.com>
2020-07-25 15:49:32 +0300

    7 12 rest test jackson ...

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,15 +1,16 @@
 package ru.javawebinar.topjava.model;
 
-import org.springframework.util.Assert;
-
 import org.hibernate.Hibernate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 
 @MappedSuperclass
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
-public abstract class AbstractBaseEntity {
+//@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
+public abstract class AbstractBaseEntity implements Persistable<Integer> {
     public static final int START_SEQ = 100000;
 
     @Id
@@ -32,6 +33,7 @@ public abstract class AbstractBaseEntity {
         this.id = id;
     }
 
+    @Override
     public Integer getId() {
         return id;
     }
@@ -42,6 +44,7 @@ public abstract class AbstractBaseEntity {
         return id;
     }
 
+    @Override
     public boolean isNew() {
         return this.id == null;
     }
