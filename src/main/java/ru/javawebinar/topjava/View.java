d9f57105edb36c6eed1c24512db8555a1a0a6b8c
buchgit <buchnorilsk@gmail.com>
2020-08-17 17:00:53 +0300

    my

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -4,4 +4,8 @@ import javax.validation.groups.Default;
 
 public class View {
     public interface Persist extends Default {}
+    public interface JsonREST {}
+    public interface JsonUI {}
+
+    public interface ValidatedUI {}
 }
